80759feb88f2fc7c027ed6863a143dc1006024fa
Faith <139481775+FaithLebepe@users.noreply.github.com>
2025-05-19 21:29:26 +0200

    Created  file structure

---
+++ src/app/components/meal-card/meal-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './meal-card.component.html',
+  styleUrl: './meal-card.component.css'
+})
+export class MealCardComponent {
+
+}
