b7403825e8c0cb207b861a8e3d44a0ff884cdd9f
Faith <139481775+FaithLebepe@users.noreply.github.com>
2025-06-14 13:50:04 +0200

    updated UI

--- src/app/pages/tabs/home/home.component.ts
+++ src/app/pages/tabs/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
-import { SpecialsComponent } from "../specials/specials.component";
 import { MenuComponent } from "../menu/menu.component";
 import { GalleryComponent } from "../gallery/gallery.component";
 import { ContactComponent } from '../contact/contact.component';
@@ -10,7 +9,7 @@ import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, SpecialsComponent, MenuComponent, GalleryComponent, ContactComponent, HeaderComponent, FooterComponent, LucideAngularModule ],
+  imports: [AboutComponent, MenuComponent, GalleryComponent, ContactComponent, HeaderComponent, FooterComponent, LucideAngularModule ],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
